4b99c8991fef046c555c6863a8852bd636e1995c
Yusef Habib <habibfernandez@gmail.com>
2016-12-04 15:03:26 +0100

    Name conventions

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    path = require('path'),
+    pug = require('pug'),
+    mongo = require('mongodb').MongoClient,
+    mongoUserPsw = process.env.MONGO_USER_PSW,
+    url = `mongodb://${mongoUserPsw}@ds119748.mlab.com:19748/fcc_api_projects`;
+    home = require('./routes/home'),
+    timestampt = require('./routes/timestampt'),
+    whoami = require('./routes/whoami'),
+    minifurl = require('./routes/minifurl'),
+    app = express(); 
+    
+
+    mongo.connect(url, (err, db) => {
+        if(err) 
+            throw err;
+        else {
+            console.log( 'Success: Connected to DB' );
+            app.use((req, res, next) => {
+                req.db = db;
+                next();
+            })
+               .use('/', home)
+               .use('/timestampt', timestampt)
+               .use('/whoami', whoami)
+               .use('/minifurl', minifurl);
+        }
+    });
+
+app.use(express.static('public'))
+   .set('views', path.join(__dirname, 'public/views'))
+   .set('view engine', 'pug');
+
+
+module.exports = app;
