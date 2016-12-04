6754b9762a2db57552f40c34a0d447c8f76f712b
Yusef Habib <habibfernandez@gmail.com>
2016-12-04 15:04:16 +0100

    Open db only one time and not one per request

--- routes/minifurl.js
+++ routes/minifurl.js
@@ -1,6 +1,4 @@
 var express = require('express'),
-    mongo = require('mongodb').MongoClient,
-    mongoUrl = "mongodb://localhost:27017/minifurl",
     router = express.Router();
 
 router
@@ -13,25 +11,22 @@ router
 router
     .route('/*')
     .get((req, res) => {        
-        var url = req.params[0];
-            
-        if(!isNormalUrl(url)) {
-            mongo.connect(mongoUrl, (err, db) => {
-                if(err) throw err;
-                db.collection('urls')
-                    .find(
-                        {minify_url: url}
-                    )
-                    .toArray((err, document) => {
-                        if(err) throw err;
-                        else  
-                            document[0] ? 
-                                res.redirect(document[0].original_url) :
-                                res.json({error: "The url is not in the DB"});
+        var url = req.params[0],
+            db = req.db,
+            collection = db.collection('minifurl');
 
-                        db.close();
-                    });
-            });
+        if(!isNormalUrl(url)) {
+            collection
+                .find(
+                    { minify_url: url }
+                )
+                .toArray((err, document) => {
+                    if(err) throw err;
+                    else  
+                        document[0] ? 
+                            res.redirect(document[0].original_url) :
+                            res.json({error: "The url is not in the DB"});
+                });
         }
 
         else {
@@ -40,10 +35,7 @@ router
                 minify_url: shortUrl(url)
             };
 
-            mongo.connect(mongoUrl, (err, db) => {
-                if(err) throw err;
-
-                db.collection('urls')
+            collection
                 .aggregate(
                     { $project: {original_url: 1, minify_url: 1, _id: 0}},
                     { $match: {original_url: doc.original_url}}
@@ -51,13 +43,10 @@ router
                 .toArray((err, documents) => {
                     if(err) throw err;
                     if(documents.length === 0)
-                        db.collection('urls').insert(doc);
+                        collection.insert(doc);
                     else    
                         res.json(documents[0]);
-
-                    db.close();
                 });
-            });
         }
     });
 
